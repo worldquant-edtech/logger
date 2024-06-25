8f0bb174cefa13c9cc65dca915d19b56b32ea61b
Andrew Plummer <plummer.andrew@gmail.com>
2024-06-25 17:06:40 +0900

    allow recording of params only by implicit inclusion

--- src/middleware.js
+++ src/middleware.js
@@ -6,10 +6,13 @@ const IGNORE_UA_REG = /^(GoogleHC|kube-probe)/;
 
 /**
  * @param {Object} [options]
+ * @param {boolean|Object[]} [options.recordParams] A flag or
+ * configuration to allow request params to be recorded.
  * @param {RegExp[]|string[]} [options.ignoreUserAgents] An array of
  * strings or regexes to test against.
  */
 export default function middleware(options) {
+  options = normalizeOptions(options);
   return (ctx, next) => {
     if (isAllowedRequest(ctx, options)) {
       const start = new Date();
@@ -24,6 +27,48 @@ export default function middleware(options) {
     }
     return next();
   };
+}
+
+function normalizeOptions(options = {}) {
+  let { recordParams } = options;
+  if (recordParams === true) {
+    recordParams = [
+      {
+        path: /.+/,
+      },
+    ];
+  } else if (recordParams) {
+    recordParams = recordParams.map((config) => {
+      return {
+        ...config,
+        include: normalizeParamList(config.include),
+      };
+    });
+  }
+  return {
+    ...options,
+    recordParams,
+  };
+}
+
+function normalizeParamList(arr) {
+  if (!arr) {
+    return;
+  }
+  // Include bases for deep arguments. For example
+  // if the include param is "user.foo" this should
+  // include both "user" and "user.foo". Note that
+  // excludes don't need this as if "user.foo" is
+  // excluded then "user" will already be included.
+  return arr.flatMap((arg) => {
+    if (typeof arg === 'string') {
+      const parts = arg.split('.');
+      return parts.map((part, i) => {
+        return parts.slice(0, i + 1).join('.');
+      });
+    }
+    return arg;
+  });
 }
 
 function isAllowedRequest(ctx, options = {}) {
@@ -70,40 +115,69 @@ function getRequestInfo(ctx) {
 const BLACKLIST = /token|password|secret|hash|jwt/i;
 
 function getRequestParams(ctx, options) {
-  if (ctx.status >= 400) {
+  const config = getRecordConfig(ctx, options);
+  if (config) {
     const { body, query } = ctx.request;
     return {
-      requestBody: strip(body, options),
-      requestQuery: strip(query, options),
+      requestBody: strip(body, config),
+      requestQuery: strip(query, config),
     };
-  } else {
-    return {};
   }
 }
 
-function strip(arg, options) {
+function getRecordConfig(ctx, options) {
+  const { recordParams = [] } = options;
+  return recordParams.find((config) => {
+    const { status = 400, path } = config;
+    const reg = getRegExp(path);
+    return ctx.status >= status && reg.test(ctx.url);
+  });
+}
+
+function strip(arg, config, path = []) {
   if (!arg || typeof arg !== 'object') {
     return arg;
   }
 
   const result = {};
   for (let [key, value] of Object.entries(arg)) {
-    if (!isBlacklisted(key, options)) {
-      result[key] = strip(value, options);
+    const p = [...path, key];
+    const fullKey = p.join('.');
+    if (canRecordParam(fullKey, config)) {
+      result[key] = strip(value, config, p);
     }
   }
   return result;
 }
 
-function isBlacklisted(key, options = {}) {
-  const { disallowedFields = [] } = options;
-  const disallowed = [BLACKLIST, ...disallowedFields];
+function canRecordParam(key, config) {
+  let { include, exclude = [] } = config;
+  exclude = [BLACKLIST, ...exclude];
 
-  return disallowed.some((el) => {
-    if (el instanceof RegExp) {
-      return el.test(key);
-    } else {
-      return el === key;
-    }
+  if (hasParam(key, include)) {
+    // Param is explicitly included.
+    return true;
+  } else {
+    // Param is not explicitly excluded
+    // and no includes exist.
+    return !include && !hasParam(key, exclude);
+  }
+}
+
+function hasParam(key, arr) {
+  if (!arr) {
+    return false;
+  }
+  return arr.some((arg) => {
+    const reg = getRegExp(arg);
+    return reg.test(key);
   });
+}
+
+function getRegExp(arg) {
+  if (arg instanceof RegExp) {
+    return arg;
+  } else {
+    return RegExp(`^${arg}$`, 'i');
+  }
 }
--- test/middleware.test.js
+++ test/middleware.test.js
@@ -41,6 +41,33 @@ function createContext(obj) {
   };
 }
 
+function runRequest(ctx, config) {
+  middleware(config)(ctx, () => {
+    jest.advanceTimersByTime(100);
+  });
+  ctx.res.end();
+}
+
+function assertBodyRecorded(expected) {
+  const [level, message] = getMessages()[0];
+  expect(level).toBe('log');
+  expect(JSON.parse(message)).toMatchObject({
+    ...(expected && {
+      requestBody: expected,
+    }),
+  });
+}
+
+function assertQueryRecorded(expected) {
+  const [level, message] = getMessages()[0];
+  expect(level).toBe('log');
+  expect(JSON.parse(message)).toMatchObject({
+    ...(expected && {
+      requestQuery: expected,
+    }),
+  });
+}
+
 describe('formatted middleware', () => {
   beforeAll(() => {
     useFormatted();
@@ -172,190 +199,6 @@ describe('google cloud middleware', () => {
     });
   });
 
-  it('should log body for errored POST request', () => {
-    const ctx = createContext({
-      url: '/foo',
-      method: 'POST',
-      status: 400,
-      request: {
-        body: {
-          bar: 'baz',
-        },
-      },
-      response: {
-        headers: {
-          'content-length': '2048',
-        },
-      },
-    });
-    middleware()(ctx, () => {
-      jest.advanceTimersByTime(100);
-    });
-    ctx.res.end();
-    const [level, message] = getMessages()[0];
-    expect(level).toBe('log');
-    expect(JSON.parse(message)).toEqual({
-      message: 'POST /foo 2KB - 100ms',
-      severity: 'INFO',
-      requestBody: {
-        bar: 'baz',
-      },
-      httpRequest: {
-        latency: '0.1s',
-        requestMethod: 'POST',
-        requestUrl: '/foo',
-        responseSize: '2048',
-        status: 400,
-      },
-    });
-  });
-
-  it('should log query for errored GET request', () => {
-    const ctx = createContext({
-      url: '/foo',
-      method: 'GET',
-      status: 400,
-      request: {
-        query: {
-          bar: 'baz',
-        },
-      },
-      response: {
-        headers: {
-          'content-length': '2048',
-        },
-      },
-    });
-    middleware()(ctx, () => {
-      jest.advanceTimersByTime(100);
-    });
-    ctx.res.end();
-    const [level, message] = getMessages()[0];
-    expect(level).toBe('log');
-    expect(JSON.parse(message)).toEqual({
-      message: 'GET /foo 2KB - 100ms',
-      severity: 'INFO',
-      requestQuery: {
-        bar: 'baz',
-      },
-      httpRequest: {
-        latency: '0.1s',
-        requestMethod: 'GET',
-        requestUrl: '/foo',
-        responseSize: '2048',
-        status: 400,
-      },
-    });
-  });
-
-  it('should not expose sensitive fields in logged body', () => {
-    const ctx = createContext({
-      url: '/foo',
-      method: 'POST',
-      status: 400,
-      request: {
-        body: {
-          bar: 'baz',
-          secret: 'BAD',
-          token: 'BAD',
-          password: 'BAD',
-          mySecret: 'BAD',
-          myToken: 'BAD',
-          myPassword: 'BAD',
-          user: {
-            bar: 'baz',
-            secret: 'BAD',
-            token: 'BAD',
-            password: 'BAD',
-            mySecret: 'BAD',
-            myToken: 'BAD',
-            myPassword: 'BAD',
-          },
-        },
-      },
-      response: {
-        headers: {
-          'content-length': '2048',
-        },
-      },
-    });
-    middleware()(ctx, () => {
-      jest.advanceTimersByTime(100);
-    });
-    ctx.res.end();
-    const [level, message] = getMessages()[0];
-    expect(level).toBe('log');
-    expect(JSON.parse(message)).toEqual({
-      message: 'POST /foo 2KB - 100ms',
-      severity: 'INFO',
-      requestBody: {
-        bar: 'baz',
-        user: {
-          bar: 'baz',
-        },
-      },
-      httpRequest: {
-        latency: '0.1s',
-        requestMethod: 'POST',
-        requestUrl: '/foo',
-        responseSize: '2048',
-        status: 400,
-      },
-    });
-  });
-
-  it('should allow customization of blacklisted fields', () => {
-    const ctx = createContext({
-      url: '/foo',
-      method: 'POST',
-      status: 400,
-      request: {
-        body: {
-          bar: 'baz',
-          test1: 'test1',
-          test2: 'test2',
-          sensitive: 'sensitive',
-          user: {
-            bar: 'baz',
-            test1: 'test1',
-            test2: 'test2',
-            sensitive: 'sensitive',
-          },
-        },
-      },
-      response: {
-        headers: {
-          'content-length': '2048',
-        },
-      },
-    });
-    middleware({
-      disallowedFields: ['sensitive', /test\d/],
-    })(ctx, () => {
-      jest.advanceTimersByTime(100);
-    });
-    ctx.res.end();
-    const [level, message] = getMessages()[0];
-    expect(level).toBe('log');
-    expect(JSON.parse(message)).toEqual({
-      message: 'POST /foo 2KB - 100ms',
-      severity: 'INFO',
-      requestBody: {
-        bar: 'baz',
-        user: {
-          bar: 'baz',
-        },
-      },
-      httpRequest: {
-        latency: '0.1s',
-        requestMethod: 'POST',
-        requestUrl: '/foo',
-        responseSize: '2048',
-        status: 400,
-      },
-    });
-  });
-
   it('should add a userId in labels for an authenticated user', () => {
     const ctx = createContext({
       url: '/foo',
@@ -422,5 +265,341 @@ describe('google cloud middleware', () => {
     middleware()(ctx, () => {});
     ctx.res.end();
     expect(getMessages()).toEqual([]);
+  });
+
+  describe('request body recording', () => {
+    it('should not record anything by default', () => {
+      const ctx = createContext({
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+          },
+        },
+      });
+      runRequest(ctx);
+      assertBodyRecorded(null);
+    });
+
+    it('should record all params', () => {
+      const ctx = createContext({
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+          },
+        },
+      });
+      runRequest(ctx, {
+        recordParams: true,
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+      });
+    });
+
+    it('should match route by string', () => {
+      const ctx = createContext({
+        url: '/foo',
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+          },
+        },
+      });
+      runRequest(ctx, {
+        recordParams: [
+          {
+            path: '/foo',
+          },
+        ],
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+      });
+    });
+
+    it('should match route by regex', () => {
+      const ctx = createContext({
+        url: '/foo',
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+          },
+        },
+      });
+      runRequest(ctx, {
+        recordParams: [
+          {
+            path: /\/foo/,
+          },
+        ],
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+      });
+    });
+
+    it('should not log below 400 status by default', () => {
+      const ctx = createContext({
+        url: '/foo',
+        status: 200,
+        request: {
+          body: {
+            bar: 'baz',
+          },
+        },
+      });
+      runRequest(ctx, {
+        recordParams: [
+          {
+            path: /\/foo/,
+          },
+        ],
+      });
+      assertBodyRecorded(null);
+    });
+
+    it('should match status below 400', () => {
+      const ctx = createContext({
+        url: '/foo',
+        status: 200,
+        request: {
+          body: {
+            bar: 'baz',
+          },
+        },
+      });
+      runRequest(ctx, {
+        recordParams: [
+          {
+            status: 200,
+            path: /\/foo/,
+          },
+        ],
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+      });
+    });
+
+    it('should log query for errored GET request', () => {
+      const ctx = createContext({
+        url: '/foo',
+        method: 'GET',
+        status: 400,
+        request: {
+          query: {
+            bar: 'baz',
+          },
+        },
+      });
+      runRequest(ctx, {
+        recordParams: true,
+      });
+      assertQueryRecorded({
+        bar: 'baz',
+      });
+    });
+
+    it('should not expose sensitive fields in body', () => {
+      const ctx = createContext({
+        url: '/foo',
+        method: 'POST',
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+            secret: 'BAD',
+            token: 'BAD',
+            password: 'BAD',
+            mySecret: 'BAD',
+            myToken: 'BAD',
+            myPassword: 'BAD',
+            user: {
+              bar: 'baz',
+              secret: 'BAD',
+              token: 'BAD',
+              password: 'BAD',
+              mySecret: 'BAD',
+              myToken: 'BAD',
+              myPassword: 'BAD',
+            },
+          },
+        },
+      });
+      runRequest(ctx, {
+        recordParams: true,
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+        user: {
+          bar: 'baz',
+        },
+      });
+    });
+
+    it('should allow customization of included fields', () => {
+      const ctx = createContext({
+        url: '/foo',
+        method: 'POST',
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+            test1: 'test1',
+            test2: 'test2',
+            sensitive: 'sensitive',
+            user: {
+              bar: 'baz',
+              test1: 'test1',
+              test2: 'test2',
+              sensitive: 'sensitive',
+            },
+          },
+        },
+        response: {
+          headers: {
+            'content-length': '2048',
+          },
+        },
+      });
+
+      runRequest(ctx, {
+        recordParams: [
+          {
+            path: '/foo',
+            include: ['bar', 'user.bar'],
+          },
+        ],
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+        user: {
+          bar: 'baz',
+        },
+      });
+    });
+
+    it('should allow exclusion of fields', () => {
+      const ctx = createContext({
+        url: '/foo',
+        method: 'POST',
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+            test1: 'test1',
+            test2: 'test2',
+            user: {
+              bar: 'baz',
+              test1: 'test1',
+              test2: 'test2',
+            },
+          },
+        },
+        response: {
+          headers: {
+            'content-length': '2048',
+          },
+        },
+      });
+
+      runRequest(ctx, {
+        recordParams: [
+          {
+            path: '/foo',
+            exclude: ['bar', 'user.bar'],
+          },
+        ],
+      });
+      assertBodyRecorded({
+        test1: 'test1',
+        test2: 'test2',
+      });
+    });
+
+    it('should not include field when excludes are set', () => {
+      const ctx = createContext({
+        url: '/foo',
+        method: 'POST',
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+            test1: 'test1',
+            test2: 'test2',
+            user: {
+              bar: 'baz',
+              test1: 'test1',
+              test2: 'test2',
+            },
+          },
+        },
+        response: {
+          headers: {
+            'content-length': '2048',
+          },
+        },
+      });
+
+      runRequest(ctx, {
+        recordParams: [
+          {
+            path: '/foo',
+            include: ['bar'],
+            exclude: ['test1'],
+          },
+        ],
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+      });
+    });
+
+    it('should exclude nested', () => {
+      const ctx = createContext({
+        url: '/foo',
+        method: 'POST',
+        status: 400,
+        request: {
+          body: {
+            bar: 'baz',
+            test1: 'test1',
+            test2: 'test2',
+            user: {
+              bar: 'baz',
+              test1: 'test1',
+              test2: 'test2',
+            },
+          },
+        },
+        response: {
+          headers: {
+            'content-length': '2048',
+          },
+        },
+      });
+
+      runRequest(ctx, {
+        recordParams: [
+          {
+            path: '/foo',
+            exclude: ['user.bar'],
+          },
+        ],
+      });
+      assertBodyRecorded({
+        bar: 'baz',
+        test1: 'test1',
+        test2: 'test2',
+        user: {
+          test1: 'test1',
+          test2: 'test2',
+        },
+      });
+    });
   });
 });
